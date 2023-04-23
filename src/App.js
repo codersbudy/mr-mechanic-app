701a349c410cd6bf22329b68a551e31123f1dfe5
Ankit Singh Rajput <mr.ankit.singh.81@gmail.com>
2023-04-23 17:49:00 +0530

    home page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return <>
+  <Navbar/>
+  <Carousel/>
+  <Footer/>
+  </>
+  
+}
+
+export default App;
