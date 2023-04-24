d18bb4746e9e2e0ef76f9b9f64172883a7b10777
Vickey <vickyhatagale505@gmail.com>
2023-04-24 14:33:13 +0530

    forget password

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
 import LogIn from "./components/loginpage/logIn";
 import Home from "./components/homepage/homepage";
@@ -8,6 +9,21 @@ function App() {
     <Route path="/" element={<Home/>}/>
     <Route path="/login" element={<LogIn/>} />
     <Route path="/forgetPassword" element={<forgetPassword/>}/>
+=======
+import { Route, Router, Routes } from "react-router-dom";
+
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/home";
+
+
+
+
+function App() {
+  return <>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+>>>>>>> 43381e976b870f8968b01488939978c7415e9367
   </Routes>
   </>
   
